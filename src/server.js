e821464de9ba1f164c0b610e12902dd6a1867553
Rodrigo Grzegorczyk <68669322+RodrigoGrz@users.noreply.github.com>
2021-03-30 11:53:53 -0300

    routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const server = express()
+const routes = require("./routes")
 
-server.get('/', (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
-})
+// habilitar arquivos statics
+server.use(express.static("public"))
+
+// routes
+server.use(routes)
 
 server.listen(3000, () => console.log('rodando'))
