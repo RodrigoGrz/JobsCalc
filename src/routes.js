13fbb1342798d57e94a2b428c9219d108cb6a1b2
Rodrigo Grzegorczyk <68669322+RodrigoGrz@users.noreply.github.com>
2021-04-02 14:18:10 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -76,7 +76,7 @@ const Job = {
                     ...job,
                     remaining,
                     status,
-                    budget: Profile.data["value-hour"] * job["total-hours"]
+                    budget: Job.services.calculateBudget(job, Profile.data["value-hour"])
                     }
                 })
             
@@ -106,9 +106,43 @@ const Job = {
 
             const jobId = req.params.id
 
-            const job = Job.data.find(job => job.id === jobId)
+            const job = Job.data.find(job => Number(job.id) === Number(jobId))
+
+            if(!job) {
+                return res.send("Job not found")
+            }
+
+            job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"])
 
             return res.render(views + "job-edit", { job })
+        },
+
+        update(req, res) {
+            const jobId = req.params.id
+
+            const job = Job.data.find(job => Number(job.id) === Number(jobId))
+
+            if(!job) {
+                return res.send("Job not found")
+            }
+
+            const updatedJob = {
+                ...job,
+                name: req.body.name,
+                "total-hours": req.body["total-hours"],
+                "daily-hours": req.body["daily-hours"],
+            }
+
+            Job.data = Job.data.map(job => {
+
+                if(Number(job.id) === Number(jobId)) {
+                    job = updatedJob
+                }
+
+                return job
+            })
+
+            res.redirect("/job/" + jobId)
         }
     },
     services: {
@@ -127,7 +161,9 @@ const Job = {
         
             // restam x dias
             return dayDiff
-        }
+        },
+
+        calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
     }
 }
 
@@ -136,6 +172,7 @@ routes.get('/', Job.controllers.index)
 routes.get('/job', Job.controllers.create)
 routes.post('/job', Job.controllers.save)
 routes.get('/job/:id', Job.controllers.show)
+routes.post('/job/:id', Job.controllers.update)
 routes.get('/profile', Profile.controllers.index)
 routes.post('/profile', Profile.controllers.update)
 
