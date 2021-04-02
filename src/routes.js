494d1e1c2da5f5c03b346483dddfa525e0fe2955
Rodrigo Grzegorczyk <68669322+RodrigoGrz@users.noreply.github.com>
2021-04-02 14:34:14 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -90,7 +90,7 @@ const Job = {
 
         save(req, res) {
         // { name: 'rthgf', 'daily-hours': '43', 'total-hours': '67' }
-        const lastId = Job.data[Job.data.length - 1] ?.id || 1;
+        const lastId = Job.data[Job.data.length - 1] ?.id || 0;
 
         Job.data.push({
             id: lastId + 1,
@@ -143,6 +143,14 @@ const Job = {
             })
 
             res.redirect("/job/" + jobId)
+        },
+
+        delete(req, res) {
+            const jobId = req.params.id
+ 
+            Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId))
+
+            return res.redirect('/')
         }
     },
     services: {
@@ -173,6 +181,7 @@ routes.get('/job', Job.controllers.create)
 routes.post('/job', Job.controllers.save)
 routes.get('/job/:id', Job.controllers.show)
 routes.post('/job/:id', Job.controllers.update)
+routes.post('/job/delete/:id', Job.controllers.delete)
 routes.get('/profile', Profile.controllers.index)
 routes.post('/profile', Profile.controllers.update)
 
