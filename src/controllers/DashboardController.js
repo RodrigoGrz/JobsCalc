025ab18187d4cd60d647bc9d577a68a13274a814
Rodrigo Grzegorczyk <68669322+RodrigoGrz@users.noreply.github.com>
2021-04-11 16:11:08 -0300

    Update

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -7,11 +7,20 @@ module.exports = {
 
         const jobs = Job.get();
         const profile = Profile.get();
+
+        let statusCount = {
+            progress: 0,
+            done: 0,
+            total: jobs.length
+        }
     
         const updatedJobs = jobs.map((job) => {
             //  ajustes no job
             const remaining = JobUtils.remainingDays(job)
             const status = remaining <= 0 ? 'done' : 'progress'
+
+            // Somando a quantidade de status
+            statusCount[status] += 1;
         
             return {
                 ...job,
@@ -22,6 +31,6 @@ module.exports = {
             })
         
             
-        return res.render("index", { jobs: updatedJobs })
+        return res.render("index", { jobs: updatedJobs, profile, statusCount })
     }
 }
